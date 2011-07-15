ffd514a394891449d05e1b6aa7a9f9457acbbae0
Scott Muc <scottmuc@gmail.com>
2011-07-15 10:51:39 -0700

    Added the ability to add non-framework build tasks
    Created dummy example of a custom build task
    
    Using custom build task

---
+++ TestProject/build.custom.tasks.ps1
@@ -0,0 +1,3 @@
+task HelloWorld {
+    "Hello World!" | Write-Host
+}
--- TestProject/lib/YDeliver/YBuild/Build.Tasks.ps1
+++ TestProject/lib/YDeliver/YBuild/Build.Tasks.ps1
@@ -1,3 +1,8 @@
 $script:dir = Split-Path -Parent $MyInvocation.MyCommand.Path
 gci $script:dir\..\CommonFunctions | % { . $_.FullName }
 gci $script:dir\Tasks | % { . $_.FullName }
+
+if (Test-Path ("$rootDir\build.custom.tasks.ps1")) {
+    "Importing custom tasks" | Write-Host
+    . "$rootDir\build.custom.tasks.ps1"
+}

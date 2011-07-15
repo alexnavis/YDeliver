6b390506899334e8918f91c426bb9524d72c5f55
Scott Muc <scottmuc@gmail.com>
2011-07-15 11:09:50 -0700

    Added tests around Get-Conventions

--- scripts/CommonFunctions/Get-Conventions.ps1
+++ scripts/CommonFunctions/Get-Conventions.ps1
@@ -1,10 +1,12 @@
-function Get-Conventions($conventions) {
+function Get-Conventions($conventions, [switch] $noexit) {
 
     $conventions | % {
         if ($ybc.$_ -eq $null) {
             "Missing convention: $_ Please update your .\build.ps1 to have a value for `$ybc.$_" | Write-Host 
-            cd $rootDir
-            exit 1
+            if (-not $noexit) {
+                cd $rootDir
+                exit 1
+            }
         }
         $ybc.$_
     }

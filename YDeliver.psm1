abf6be2e2464c5eb19b9eeb2919626f65aaebf27
Scott Muc <scottmuc@gmail.com>
2011-10-02 15:33:50 -0700

    Removed YAML dependency

--- YDeliver.psm1
+++ YDeliver.psm1
@@ -1,20 +1,17 @@
 Import-Module "$PSScriptRoot\lib\PSake\psake.psm1" -Force
-Import-Module "$PSScriptRoot\lib\PowerYaml\PowerYaml.psm1" -Force
 
 function Invoke-YBuild {
     [CmdletBinding()]
     param($tasks, $buildVersion = "1.0.0", $rootDir = $pwd)
 
     . "$PSScriptRoot\Config\Default.Conventions.ps1"
-    $config = Get-Yaml -FromFile "$rootDir\build.yml"
 
     Invoke-Psake "$PSScriptRoot\YBuild\Build.Tasks.ps1" `
-        -framework $config.dotnet.framework_version `
+        -framework "4.0x64" `
         -taskList $tasks `
         -parameters @{
             "buildVersion" = $buildVersion;
             "ybc" = $ybc;
-            "config" = $config;
             "rootDir" = $rootDir;
           }
 }

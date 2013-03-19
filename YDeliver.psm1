748ae865dc1f8e80b72798512d78bd3ee9b30aac
Sanjana Bayya <sanjanb@thoughtworks.com>
2013-03-19 18:25:37 +0530

    Sanjana|added paramater to Invoke YBuild to take isRemoteDBOn

--- YDeliver.psm1
+++ YDeliver.psm1
@@ -6,20 +6,22 @@ Import-Module "$PSScriptRoot\lib\PowerYaml\PowerYaml.psm1" -Force
 
 function Invoke-YBuild {
     [CmdletBinding()]
-    param([string[]] $tasks = @('Help'), $buildVersion = "1.0.0", $rootDir = $pwd)
+    param([string[]] $tasks = @('Help'), $buildVersion = "1.0.0", $rootDir = $pwd,$isRemoteDBOn)
 
     # some hackery to provide the rootDir to Build.Tasks.ps1
     $global:rootDir = $rootDir
     . "$PSScriptRoot\Conventions\Defaults.ps1"
+     $global:isRemoteDBOn=$isRemoteDBOn
 
     Invoke-Psake "$PSScriptRoot\YBuild\Build.Tasks.ps1" `
         -framework $conventions.framework `
         -taskList $tasks `
         -parameters @{
             "buildVersion" = $buildVersion;
-            "buildConfig" =  (Get-BuildConfiguration $rootDir); 
+            "buildConfig" =  (Get-BuildConfiguration $rootDir);
             "conventions" = $conventions;
             "rootDir" = $rootDir;
+            "isRemoteDBOn" = $isRemoteDBOn
           }
 }
 
